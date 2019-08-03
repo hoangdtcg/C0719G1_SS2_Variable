09bdf36a1c6aa2f9a01cfb45a567919b8b735c6d
Nguyễn Như Công <53465166+nhok9xyeuem@users.noreply.github.com>
2019-08-03 16:51:07 +0700

    thuc hanh if else
    a ơi !~
    em thắc mắc là tại sao khi e gán điều kiện tk or mk trong bài == null mà khi chạy e k thấy nó k có tác dụng gi ! kiểu như bỏ qua luôn đoạn mã đó a ak ?????

---
+++ th-If-Else-If/if-else-if.js
@@ -0,0 +1,26 @@
+function dangnhap()
+{
+    let tk =(document.getElementById('tendangnhap').value);
+    let mk=(document.getElementById('matkhau').value);
+
+    if (tk=='admin') {
+        if(mk=='admin'){
+            document.getElementById('tk').innerHTML= 'chao mung ban dang nhap thanh cong';
+        }
+        else if (mk==null){
+            document.getElementById('tk').innerHTML="moi ban nhap lai mat khau"
+        }
+        else {
+            document.getElementById('tk').innerHTML='tai khoan hoac mat khau khong dung'
+        }
+
+    }
+    else if (tk==null){
+        document.getElementById("tk").innerHTML="moi ban nhap lai tai khoan dang nhap"
+
+    }
+    else {
+        document.getElementById('tk').innerHTML="tai khoan hoac mat khau khong dung"
+    }
+
+   }
